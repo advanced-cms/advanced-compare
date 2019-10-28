09a0156c34f2dcc7eef0be964c2b6bdce139fc69
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2019-10-28 10:08:57 +0100

    Create start page versions generator

---
+++ src/Alloy/Business/Plugins/ContentVersionGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Alloy.Models.Blocks;
+using Alloy.Models.Pages;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAccess;
+using EPiServer.Framework;
+using EPiServer.Security;
+
+namespace Alloy.Business.Plugins
+{
+    public class ContentVersionGenerator
+    {
+        private readonly IContentRepository _contentRepository;
+        private readonly IContentVersionRepository _contentVersionRepository;
+
+        public ContentVersionGenerator(IContentRepository contentRepository, IContentVersionRepository contentVersionRepository)
+        {
+            _contentRepository = contentRepository;
+            _contentVersionRepository = contentVersionRepository;
+        }
+
+        public IContent CreateContentVersion(ContentReference contentLink, DateTime versionDate, string data1, string data2, string data3)
+        {
+            ContextCache.Current["CurrentITransferContext"] = (object)this;
+            ContextCache.Current["PageSaveDB:PageSaved"] = (object)true;
+
+            var content = _contentRepository.Get<IContent>(contentLink);
+
+
+            var saveAction = SaveAction.Publish | SaveAction.SkipValidation;
+            var command = saveAction & SaveAction.ActionMask;
+            SaveAction action = (content is IVersionable ? command : SaveAction.Default) | (SaveAction.ForceCurrentVersion | saveAction & SaveAction.SkipValidation);
+            AccessLevel requiredDestinationAccess = AccessLevel.NoAccess;
+
+            var contentData = content as ContentData;
+            contentData = (ContentData) contentData.CreateWritableClone();
+
+            IChangeTrackable changeTrackable = contentData as IChangeTrackable;
+            changeTrackable.Saved = versionDate;
+            changeTrackable.Changed = versionDate;
+            changeTrackable.Created = versionDate;
+            changeTrackable.SetChangedOnPublish = true;
+
+            IContent contentClone = (IContent)contentData;
+            contentClone.ContentLink = contentLink.ToReferenceWithoutVersion();
+            contentClone.ContentGuid = Guid.Empty;
+
+            if (contentClone is EditorialBlock)
+            {
+                contentClone.Property["MainBody"].Value = new XhtmlString(data1);
+            }
+            else if (contentClone is StandardPage)
+            {
+                contentClone.Property["PageName"].Value = data1;
+                contentClone.Property["MetaDescription"].Value = data2;
+                var contentArea = new ContentArea();
+                var contentLinks = data3.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var link in contentLinks)
+                {
+                    contentArea.Items.Add(new ContentAreaItem
+                    {
+                        ContentLink = new ContentReference(link)
+                    });
+                }
+                contentClone.Property["MainContentArea"].Value = contentArea;
+            }
+
+            (contentClone as IVersionable).StartPublish = versionDate;
+
+            SaveAction saveAction2 = (SaveAction.SkipValidation | SaveAction.Publish) & SaveAction.ActionMask;
+            var contentReference = _contentRepository.Save(contentClone, saveAction2, requiredDestinationAccess);
+
+            return content;
+        }
+    }
+}
---
+++ src/Alloy/Business/Plugins/StartPageVersionGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using Alloy.Models.Blocks;
+using Alloy.Models.Pages;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAccess;
+using EPiServer.Framework;
+using EPiServer.Security;
+
+namespace Alloy.Business.Plugins
+{
+    public class StartPageVersionGenerator
+    {
+        private readonly IContentRepository _contentRepository;
+
+        public StartPageVersionGenerator(IContentRepository contentRepository, IContentVersionRepository contentVersionRepository)
+        {
+            _contentRepository = contentRepository;
+        }
+
+        public void CreateVersions()
+        {
+            AccessLevel requiredDestinationAccess = AccessLevel.NoAccess;
+
+            ContextCache.Current["CurrentITransferContext"] = (object)this;
+            ContextCache.Current["PageSaveDB:PageSaved"] = (object)true;
+
+            var startPage = _contentRepository.Get<StartPage>(ContentReference.StartPage);
+
+            var jumbotronBlock = startPage.MainContentArea.Items
+                .Select(x => _contentRepository.Get<IContent>(x.ContentLink)).OfType<JumbotronBlock>().FirstOrDefault();
+
+            // create versions for Jumbotron block
+            var currentDate = DateTime.Now;
+            for (var i = 1; i < 30; i++)
+            {
+                var clone = (JumbotronBlock)jumbotronBlock.CreateWritableClone();
+                var newDate = currentDate.AddMonths(-i);
+                clone.ButtonText = clone.ButtonText + " " + newDate.ToString("yyyy MMM");
+                clone.Heading = clone.Heading + " " + newDate.ToString("yyyy MMM");
+
+                var changeTrackable = clone as IChangeTrackable;
+                changeTrackable.Saved = newDate;
+                changeTrackable.Changed = newDate;
+                changeTrackable.Created = newDate;
+                changeTrackable.SetChangedOnPublish = true;
+
+                IContent contentData = (IContent)clone;
+                contentData.ContentLink = contentData.ContentLink.ToReferenceWithoutVersion();
+                contentData.ContentGuid = Guid.Empty;
+
+                (clone as IVersionable).StartPublish = newDate;
+
+                SaveAction saveAction2 = (SaveAction.SkipValidation | SaveAction.Publish) & SaveAction.ActionMask;
+                var contentReference = _contentRepository.Save(contentData, saveAction2, requiredDestinationAccess);
+            }
+
+            // create versions for Start Page
+            for (var i = 1; i < 15; i++)
+            {
+                var clone = (StartPage)startPage.CreateWritableClone();
+                var newDate = currentDate.AddMonths(-i * 2).AddDays(-1);
+                clone.Name = clone.Name + " " + newDate.ToString("yyyy-MM-dd");
+
+                var changeTrackable = clone as IChangeTrackable;
+                changeTrackable.Saved = newDate;
+                changeTrackable.Changed = newDate;
+                changeTrackable.Created = newDate;
+                changeTrackable.SetChangedOnPublish = true;
+
+                IContent contentData = (IContent)clone;
+                contentData.ContentLink = contentData.ContentLink.ToReferenceWithoutVersion();
+                contentData.ContentGuid = Guid.Empty;
+
+                (clone as IVersionable).StartPublish = newDate;
+
+                SaveAction saveAction2 = (SaveAction.SkipValidation | SaveAction.Publish) & SaveAction.ActionMask;
+                var contentReference = _contentRepository.Save(contentData, saveAction2, requiredDestinationAccess);
+            }
+        }
+    }
+}
